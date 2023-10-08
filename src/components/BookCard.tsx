4bb2478634ddb86b8a42749ea2ec8f7bbe747604
Springhawk27 <sajjad0mahmud@gmail.com>
2023-10-08 19:00:31 +0600

    few adjustments

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -30,34 +30,36 @@ const BookCard = ({ book }: IProps) => {
   };
 
   return (
-    <div className="card w-auto bg-base-100 shadow-xl">
-      <div className="card-body items-center text-center">
-        <h2 className="card-title"> {book.title}</h2>
-        <p>by {book.author}</p>
-        <p>Genre: {book.genre}</p>
-        <p>Publication Date: {book.publicationDate}</p>
-        <div className="card-actions flex flex-col justify-center items-center space-y-2">
-          <Link to={`/book-detail/${book?._id}`} className="btn btn-primary">
-            Details
-          </Link>
-          <div className="space-x-2">
-            <button
-              onClick={handleAddToWishlist}
-              className="btn btn-small text-xs border-none"
-            >
-              Add to Wishlist
-            </button>
-            <button
-              onClick={handleAddToReadingList}
-              className="btn btn-small text-xs border-none"
-            >
-              Add to Reading List
-            </button>
+    <>
+      <div className="card w-auto bg-base-100 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title"> {book.title}</h2>
+          <p>by {book.author}</p>
+          <p>Genre: {book.genre}</p>
+          <p>Publication Date: {book.publicationDate}</p>
+          <div className="card-actions flex flex-col justify-center items-center space-y-2">
+            <Link to={`/book-detail/${book?._id}`} className="btn btn-primary">
+              Details
+            </Link>
+            <div className="space-x-2 space-y-2">
+              <button
+                onClick={handleAddToWishlist}
+                className="btn btn-small text-xs border-none"
+              >
+                Add to Wishlist
+              </button>
+              <button
+                onClick={handleAddToReadingList}
+                className="btn btn-small text-xs border-none"
+              >
+                Add to Reading List
+              </button>
+            </div>
           </div>
         </div>
+        <Toaster position="top-center" />
       </div>
-      <Toaster position="top-center" />
-    </div>
+    </>
   );
 };
 
--- src/components/WishlistCard.tsx
+++ src/components/WishlistCard.tsx
@@ -1,5 +1,10 @@
+import { addToReadingList } from '@/redux/features/books/readingListSlice';
+import { removeFromWishList } from '@/redux/features/books/wishListSlice';
+import { useAppDispatch } from '@/redux/hook';
 import { IBook } from '@/types/globalTypes';
 import { Link } from 'react-router-dom';
+import { Toaster, toast } from 'react-hot-toast';
+import { useState } from 'react';
 
 interface IProps {
   book: IBook;
@@ -7,6 +12,17 @@ interface IProps {
 
 const WishlistCard = ({ book }: IProps) => {
   // console.log(book);
+  const dispatch = useAppDispatch();
+  const [toastMessage, setToastMessage] = useState('Added to Reading List');
+
+  const handleRemoveFromWishlist = () => {
+    dispatch(removeFromWishList(book));
+  };
+  const handleAddToReadingList = () => {
+    dispatch(addToReadingList(book));
+    setToastMessage('Added to Reading List');
+    toast.success(toastMessage);
+  };
 
   return (
     <div className="card w-auto bg-base-100 shadow-xl">
@@ -15,12 +31,27 @@ const WishlistCard = ({ book }: IProps) => {
         <p>by {book.author}</p>
         <p>Genre: {book.genre}</p>
         <p>Publication Date: {book.publicationDate}</p>
-        <div className="card-actions">
+        <div className="card-actions flex flex-col justify-center items-center">
           <Link to={`/book-detail/${book?._id}`} className="btn btn-primary">
             Details
           </Link>
+          <div className="space-x-2 space-y-2 ">
+            <button
+              onClick={handleRemoveFromWishlist}
+              className="btn btn-small text-xs border-none"
+            >
+              Remove from Wishlist
+            </button>
+            <button
+              onClick={handleAddToReadingList}
+              className="btn btn-small text-xs border-none"
+            >
+              Add to Reading List
+            </button>
+          </div>
         </div>
       </div>
+      <Toaster position="top-center" />
     </div>
   );
 };
